e4dbe3bf5be221ff7fb28bdae667e91a8a3cfb82
Robert Chettleburgh <rob@evosite.co.uk>
2013-04-10 21:44:03 +0100

    - Added support for combined titles for field link

--- inc/object/form/field_link.php
+++ inc/object/form/field_link.php
@@ -9,12 +9,12 @@ class field_link extends field {
         $this->attributes['type'] = 'number';
     }
 
-    public function get_database_create_query(){
+    public function get_database_create_query() {
         return 'int(6)';
     }
 
     public function get_html() {
-        if(!$this->hidden) {
+        if (!$this->hidden) {
             return '<select ' . $this->get_attributes() . '>' . $this->get_options() . '</select>' . "\n";
         } else {
             return parent::get_html();
@@ -24,25 +24,44 @@ class field_link extends field {
     public function get_options() {
         $html = '';
         $class = core::get_class_from_mid($this->link_mid);
-        $field_name = core::get_field_from_fid($this->link_fid);
         $obj = new $class();
-        $options = $class::get_all(array($field_name,$obj->table_key), $this->options);
-        if(!$this->required) {
+        if (is_numeric($this->link_fid)) {
+            $this->link_fid = core::get_field_from_fid($this->link_fid);
+        }
+        if (is_array($this->link_fid)) {
+            $fields = $this->link_fid;
+            $fields[] = $obj->table_key;
+        } else {
+            $fields = array($obj->table_key, $this->link_fid);
+        }
+        $options = $class::get_all($fields, $this->options);
+        if (!$this->required) {
             $html .= '<option value="0">- Please Select -</option>';
         }
-        $selected = isset($this->parent_form->{$this->field_name}) ? $this->parent_form->{$this->field_name} : 0 ;
-        $options->iterate(function(table $object) use (&$html, $field_name, $selected) {
-            $html .= '<option value="' . $object->{$object->table_key} . '" ' . ($object->{$object->table_key} == $selected ? 'selected="selected"' : '') . '>' . $object->{$field_name}  . '</option>';
-        });
+        $title_fields = $this->link_fid;
+        $selected = isset($this->parent_form->{$this->field_name}) ? $this->parent_form->{$this->field_name} : 0;
+        $options->iterate(function (table $object) use (&$html, $title_fields, $selected) {
+                if (is_array($title_fields)) {
+                    $parts = array();
+                    foreach ($title_fields as $part) {
+                        $parts[] = $object->{str_replace('.','_',$part)};
+                    }
+                    $title = implode(' - ', $parts);
+                } else {
+                    $title = $object->$title_fields;
+                }
+                $html .= '<option value="' . $object->{$object->table_key} . '" ' . ($object->{$object->table_key} == $selected ? 'selected="selected"' : '') . '>' . $title . '</option>';
+            }
+        );
         return $html;
     }
 
-    public function  get_cms_list_wrapper($value, $object_class ,$id) {
+    public function  get_cms_list_wrapper($value, $object_class, $id) {
         $class = core::get_class_from_mid($this->link_mid);
         $field_name = core::get_field_from_fid($this->link_fid);
         $object = new $class();
         $object->do_retrieve_from_id(array($field_name, $object->table_key), $value);
-        return (isset($object->{$object->table_key}) && $object->{$object->table_key} ?$object->$field_name : '-');
+        return (isset($object->{$object->table_key}) && $object->{$object->table_key} ? $object->$field_name : '-');
     }
 
 }
