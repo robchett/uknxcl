f1d004eadb23b9ab8fdde5a2cc82996ddb4a9dfd
Robert Chettleburgh <rob@evosite.co.uk>
2013-04-11 19:53:25 +0100

    - Add an error class to the fields wrapping li - Add the correct class name to action for forms created form table objects

--- inc/object/form/form.php
+++ inc/object/form/form.php
@@ -29,7 +29,7 @@ class form extends html_element {
             if (get_class($field) == 'field_file') {
                 $this->use_ajax = false;
                 $this->action = '/index.php?module=' . get_class($this) . '&act=do_submit&no_ajax=on&ajax_origin=' . $this->id;
-                $this->attributes['target'] = 'form_target_' .  get_class($this);
+                $this->attributes['target'] = 'form_target_' . get_class($this);
                 $this->attributes['enctype'] = 'multipart/form-data';
                 break;
             }
@@ -51,6 +51,7 @@ class form extends html_element {
                 $this->{$field->field_name} = $object->{$field->field_name};
             }
         }
+        $this->action = get_class($object) . ':do_submit';
     }
 
     public function remove_field($field_name) {
@@ -93,6 +94,26 @@ class form extends html_element {
         return false;
     }
 
+    public function do_validate() {
+        foreach ($this->fields as $field) {
+            $field->do_validate($this->validation_errors);
+        }
+    }
+
+    public function do_invalidate_form() {
+        $html = '<ul class="error_list">';
+        foreach ($this->validation_errors as $key => $val) {
+            $field = $this->get_field_from_name($key);
+            $field->add_class('err');
+            $field->add_wrapper_class('err');
+            $html .= '<li>' . $key . ' ' . $val . '</li>';
+        }
+
+        $html .= '</ul>';
+        ajax::update($this->get_html()->get());
+        //  ajax::inject('#' . $this->id, 'prepend', $html, '#' . $this->id . '_error');
+    }
+
     public function get_html() {
         $this->attributes = array_merge(array(
                 'name' => $this->id,
@@ -112,7 +133,7 @@ class form extends html_element {
         return $html;
     }
 
-    public function get_html_body(){
+    public function get_html_body() {
         $form = html_node::create('form#' . $this->id . '.' . ($this->use_ajax ? 'ajax' : 'noajax'), '', $this->attributes);
         if (!empty($this->pre_fields_text)) {
             $form->nest(html_node::create('div.pre_fields_text', $this->pre_fields_text));
@@ -134,65 +155,41 @@ class form extends html_element {
         return $form;
     }
 
-        public
-        function get_fields_html() {
-            $fieldsets = array();
-            $fields = array();
-            foreach ($this->fields as $field) {
-                if (!$field->hidden) {
-                    if (isset($field->fieldset)) {
-                        $fieldsets[] = html_node::create('fieldset.fieldset_' . count($fieldsets) . ' ul')->nest($fields);
-                        $fields = array();
-                    }
-                    if($inner = $field->get_html_wrapper()) {
-                    $fields[] = html_node::create('li.' . get_class($field) . '_wrapper', $inner, array('data-for' => $this->id, 'class' => $field->get_wrapper_class()));
-                    }
+    public function get_fields_html() {
+        $fieldsets = array();
+        $fields = array();
+        foreach ($this->fields as $field) {
+            if (!$field->hidden) {
+                if (isset($field->fieldset)) {
+                    $fieldsets[] = html_node::create('fieldset.fieldset_' . count($fieldsets) . ' ul')->nest($fields);
+                    $fields = array();
+                }
+                if ($inner = $field->get_html_wrapper()) {
+                    $fields[] = html_node::create('li.' . $field->get_wrapper_class(), $inner, array('data-for' => $this->id));
                 }
             }
-            $fieldsets[] = html_node::create('fieldset.fieldset_' . count($fieldsets) . ' ul')->nest($fields);
-            return $fieldsets;
         }
-
-        public
-        function get_hidden_fields() {
-            $html = html_node::create('ul.hidden');
-            foreach ($this->fields as $field) {
-                if ($field->hidden)
-                    $html->add_child(html_node::create('li', $field->get_html_wrapper(), array('data-for' => $this->id, 'class' => $field->get_wrapper_class())));
-            }
-            return $html;
-        }
-
-        public
-        function get_submit() {
-            if ($this->has_submit) {
-                $field = html_node::create('input.submit', '', array('type' => 'submit', 'data-for' => $this->id, 'name' => $this->submit));
-                if (!$this->submittable) {
-                    $field->add_attribute('disabled', 'disabled');
-                }
-                return $field;
-            }
-            return html_node::create('');
-        }
-
-        public
-        function do_validate() {
-            foreach ($this->fields as $field) {
-                $field->do_validate($this->validation_errors);
-            }
-        }
-
-        public
-        function do_invalidate_form() {
-            $html = '<ul class="error_list">';
-            foreach ($this->validation_errors as $key => $val) {
-                $field = $this->get_field_from_name($key);
-                $field->add_class('err');
-                $html .= '<li>' . $key . ' ' . $val . '</li>';
-            }
-
-            $html .= '</ul>';
-            ajax::update($this->get_html()->get());
-            //  ajax::inject('#' . $this->id, 'prepend', $html, '#' . $this->id . '_error');
-        }
+        $fieldsets[] = html_node::create('fieldset.fieldset_' . count($fieldsets) . ' ul')->nest($fields);
+        return $fieldsets;
     }
+
+    public function get_hidden_fields() {
+        $html = html_node::create('ul.hidden');
+        foreach ($this->fields as $field) {
+            if ($field->hidden)
+                $html->add_child(html_node::create('li', $field->get_html_wrapper(), array('data-for' => $this->id, 'class' => $field->get_wrapper_class())));
+        }
+        return $html;
+    }
+
+    public function get_submit() {
+        if ($this->has_submit) {
+            $field = html_node::create('input.submit', '', array('type' => 'submit', 'data-for' => $this->id, 'name' => $this->submit));
+            if (!$this->submittable) {
+                $field->add_attribute('disabled', 'disabled');
+            }
+            return $field;
+        }
+        return html_node::create('');
+    }
+}
