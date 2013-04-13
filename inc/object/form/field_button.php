78acf8116fcecc80b00b497e23c4900087b5475d
Robert Chettleburgh <rob@evosite.co.uk>
2013-04-13 18:21:58 +0100

    - Added a loading shroud to cms button fields

--- inc/object/form/field_button.php
+++ inc/object/form/field_button.php
@@ -12,7 +12,8 @@ class field_button extends field {
     public function get_cms_list_wrapper($value, $object_class, $id) {
         $this->attributes['data-ajax-click'] = $object_class . ':' . $this->field_name;
         $this->attributes['data-ajax-post'] = '{"id":' . $id . '}';
-        return html_node::create('a.button', $this->title, $this->attributes)->get();
+        $this->attributes['data-ajax-shroud'] = '#button' . $this->field_name. $id;
+        return html_node::create('a#button' . $this->field_name. $id .'.button', $this->title, $this->attributes)->get();
     }
 
     public function get_save_sql(&$sql_array, &$parameters) {
