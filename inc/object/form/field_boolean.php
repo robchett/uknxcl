f024b37319ba5b1ecbded188bea999f3180d2812
Robert Chettleburgh <rob@evosite.co.uk>
2013-04-16 19:01:21 +0100

    - fixed ajax shroud on cms field_boolean

--- inc/object/form/field_boolean.php
+++ inc/object/form/field_boolean.php
@@ -21,7 +21,7 @@ class field_boolean extends field {
         $this->attributes['data-ajax-click'] = $object_class . ':do_cms_update';
         $this->attributes['data-ajax-post'] = '{"field":"' . $this->field_name . '", "value":' . (int) !$this->parent_form->{$this->field_name} . ',"id":' . $id . '}';
         $this->attributes['id'] = (isset($this->attributes['id']) ? $this->attributes['id'] : $this->field_name) . '_' . $id;
-        $this->attributes['data-ajax-shroud'] = '{"field":"' . $this->field_name . '", "value":' . (int) !$this->parent_form->{$this->field_name} . ',"id":' . $id . '}';
+        $this->attributes['data-ajax-shroud'] = '#' . $this->field_name . '_' . $this->parent_form->{$this->parent_form->table_key};
         return $this->get_html();
     }
 
