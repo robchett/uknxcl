2d8ecd580d60586940f09d085b95ca449ad4c895
Robert Chettleburgh <rob@evosite.co.uk>
2013-04-18 18:48:45 +0100

    - Limit colourbox sizings

--- .core/classes/jquery.php
+++ .core/classes/jquery.php
@@ -2,7 +2,7 @@
 
 class jquery {
 
-    public static $colourbox_defaults  = array('opacity'=>0.7, 'static' =>1);
+    public static $colourbox_defaults  = array('opacity'=>0.7, 'static' =>1, 'maxWidth' => '85%', 'maxHeight'=>'85%');
     public static function colorbox($options = array()) {
         $options = array_merge($options, self::$colourbox_defaults);
         ajax::inject('body', 'append', '<script>$.colorbox(' . json_encode($options) . ')</script>');
