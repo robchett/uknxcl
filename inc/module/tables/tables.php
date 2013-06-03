e174b5d62ece8b3e12ce7976e515a7eabf3401db
Robert Chettleburgh <rob@evosite.co.uk>
2013-06-03 20:49:33 +0100

    - Checkbox styling - Table key styling - Better use of LESS mixins for borders (@blue_border)

--- inc/module/tables/tables.php
+++ inc/module/tables/tables.php
@@ -15,16 +15,19 @@ class tables extends core_module {
         $html .= $form->get_html()->get();
         $form = new table_gen_form();
         $html .= $form->get_html()->get();
-        $html .= '<div class="key_switch">Key (click to show/hide)</div>';
-        $html .= '<div id="key2" style="display:none">
-    <b>KML prefixes:</b>- is no trace, = shows 2D, &#8801; shows 3D <br/>
-    <b>Launch prefixes: </b> A = Aerotow, W = Winch, else Foot <br/>
-    <b>Colours:</b><a style=\'color:black\'> Open Dist</a> <a style=\'color:green\'>Out & Return</a> <a
-        style=\'color:red\'>Goal</a> <a style=\'color:blue\'>Triangle</a>
-</div>
+        $html .= '
+        <div class="key_switch">
+            <span class="">Key</span>
+            <div id="key2">
+                <b>KML prefixes:</b>- is no trace, = shows 2D, &#8801; shows 3D <br/>
+                <b>Launch prefixes: </b> A = Aerotow, W = Winch, else Foot <br/>
+                <b>Colours:</b>
+                <a style=\'color:black\'> Open Dist</a>
+                <a style=\'color:green\'>Out & Return</a>
+                <a style=\'color:red\'>Goal</a> <a style=\'color:blue\'>Triangle</a>
+            </div>
+        </div>
 <div id="generated_tables">' . $table->get_table() . '</div>';
-
-        core::$inline_script[] = '$(".key_switch").click(function($("#key2").toggle()));';
 
         return $html;
     }
