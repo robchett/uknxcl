04560da605638155959682ff9e7302ea84537772
Robert Chettleburgh <rob@evosite.co.uk>
2013-06-05 20:24:34 +0100

    - Change cms titles and blurbs

--- inc/module/cms/view/dashboard.php
+++ inc/module/cms/view/dashboard.php
@@ -4,8 +4,7 @@ class dashboard_view extends cms_view {
     public function get_view() {
         $html = html_node::create('div')->nest(array(
                 html_node::create('h2', 'Welcome to the dashboard'),
-                html_node::create('p', 'From here you will able to edit pretty much anything on the site, currently a work in progress so only the parts below will be editable for now'),
-                html_node::create('div#summaries.cf')->nest(
+                 html_node::create('div#summaries.cf')->nest(
                     array(
                         html_node::create('div')->nest(
                             array(
--- inc/module/cms/view/module.php
+++ inc/module/cms/view/module.php
@@ -2,8 +2,7 @@
 class module_view extends cms_view {
     public function get_view() {
         $html = html_node::create('div')->nest(array(
-                html_node::create('h2', 'Welcome to the dashboard'),
-                html_node::create('p', 'From here you will able to edit pretty much anything on the site, currently a work in progress so only the parts below will be editable for now'),
+                html_node::create('h2', 'View all ' . get_class($this->module->current) . 's'),
                 $this->module->get_admin_edit(),
                 $this->module->get_admin_add(),
                 $this->module->get_inner(),
