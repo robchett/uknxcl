1cf1391b49a52b02522f6f03143dd3aa7571bb47
Robert Chettleburgh <rob@evosite.co.uk>
2013-06-07 18:58:34 +0100

    - Add more detail to the module list and style it

--- inc/module/cms/view/module_list.php
+++ inc/module/cms/view/module_list.php
@@ -12,14 +12,35 @@ class module_list_view extends cms_view {
 
     public function get_module_list() {
         $html = html_node::create('div');
-        $modules = _cms_modules::get_all(array('mid', 'title'));
+        $modules = _cms_modules::get_all(array('mid', 'title','primary_key', '_cms_group.title', 'table_name'), array('join'=>array('_cms_group'=>'_cms_group.gid = _cms_modules.gid')));
         if ($modules) {
-            $ul = html_node::create('ul');
-            $modules->iterate($t = function ($module) use ($ul) {
-                    $ul->nest(html_node::create('li', $module->mid . '-' . $module->title));
+            $table = html_node::create('table.module');
+            $table->nest(
+                html_node::create('thead')->nest(
+                    array(
+                        html_node::create('th', 'Module ID'),
+                        html_node::create('th', 'Group'),
+                        html_node::create('th', 'Title'),
+                        html_node::create('th', 'Table Name'),
+                        html_node::create('th', 'Primary Key'),
+                    )
+                )
+            );
+            $modules->iterate($t = function ($module) use ($table) {
+                    $table->nest(
+                        html_node::create('tr')->nest(
+                            array(
+                                html_node::create('td', $module->mid),
+                                html_node::create('td', $module->_cms_group_title),
+                                html_node::create('td', $module->title),
+                                html_node::create('td', $module->table_name),
+                                html_node::create('td', $module->primary_key),
+                            )
+                        )
+                    );
                 }
             );
-            $html->nest($ul);
+            $html->nest($table);
         }
         return $html;
     }
