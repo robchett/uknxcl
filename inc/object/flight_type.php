f8187788555b213e240b3b10ab8fb0ac7a7c62f1
Robert Chettleburgh <rob@evosite.co.uk>
2013-06-17 19:37:18 +0100

    - Add defines for flight type IDs

--- inc/object/flight_type.php
+++ inc/object/flight_type.php
@@ -1,14 +1,38 @@
 <?php
 class flight_type extends table {
+
+    const OD_ID = 1;
+    const OR_ID = 2;
+    const GO_ID = 3;
+    const TR_ID = 4;
+    const FT_ID = 5;
+
     public $table_key = 'ftid';
     public static $module_id = 10;
     public $ftid;
     public $fn;
     public $title;
 
+    protected static $all_rows;
+
     /* @return flight_type_array */
     public static function get_all(array $fields, array $options = array()) {
         return flight_type_array::get_all($fields, $options);
+    }
+
+    public static function get_multiplier($type, $season, $defined = false) {
+        if(!isset(self::$all_rows)) {
+            self::$all_rows = flight_type::get_all(array());
+        }
+        foreach (self::$all_rows as $ftype) {
+            if($ftype->ftid == $type) {
+                if($season < 2001) {
+                    return $defined ? $ftype->multi_defined_old : $ftype->multi_old ;
+                } else {
+                    return $defined ? $ftype->multi_defined : $ftype->multi;
+                }
+            }
+        }
     }
 }
 
