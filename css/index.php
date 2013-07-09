6f54eaed057feddb47c2a07e524cc9ca8d4f41c1
Robert Chettleburgh <rob@evosite.co.uk>
2013-07-09 22:58:11 +0100

    - Compile less with lessc

--- css/index.php
+++ css/index.php
@@ -1,9 +1,14 @@
 <?php
-
+require_once('../index.php');
 header("Content-type: text/css");
 $css = '';
-$files = glob('*.css');
+$files = glob('*.less');
 foreach ($files as $file) {
-    $css .= file_get_contents($file);
+    $css .= '@import "' . pathinfo($file, PATHINFO_FILENAME) . "\";\n";
 }
-echo $css;
+lessc::$debug = true;
+$lessc = new lessc();
+$lessc->addImportDir(root . '/css/');
+$less = $lessc->parse($css, null, 'dynamic_import.less');
+//$less = lessc::cexecute($css, null);
+echo $less;
