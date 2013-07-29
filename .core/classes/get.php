aeae41f6a0cd8e181994021b4bd1db716318d333
Robert Chettleburgh <rob@evosite.co.uk>
2013-07-29 19:45:01 +0100

    Add ini file parsings to get settings.

--- .core/classes/get.php
+++ .core/classes/get.php
@@ -2,6 +2,8 @@
 
 class get {
 
+    private static $settings;
+
     static function ent($html) {
         return htmlentities(html_entity_decode($html));
     }
@@ -173,4 +175,33 @@ class get {
         }
     }
 
+    public static function ini($key, $block = 'site', $default = null) {
+        if (!isset(self::$settings)) {
+            if (is_readable(root . '/.conf/config.ini')) {
+                self::$settings = parse_ini_file(root . '/.conf/config.ini', true);
+            } else {
+                throw new Exception('Could not find ini file.');
+            }
+            if (defined('host') && is_readable(root . '/.conf/' . host . '.ini')) {
+                $sub_settings = parse_ini_file(root . '/.conf/' . host . '.ini', true);
+                foreach($sub_settings as $ini_block => $ini_keys) {
+                    if(isset(self::$settings[$ini_block])) {
+                        self::$settings[$ini_block] = $ini_keys;
+                    } else {
+                        self::$settings[$ini_block] = array_merge(self::$settings[$ini_block], $ini_keys);
+                    }
+                }
+            }
+        }
+
+
+        if (isset(self::$settings[$block][$key])) {
+            return self::$settings[$block][$key];
+        } else if (isset($default)) {
+            return $default;
+        } else {
+            throw new Exception('Setting not found and no default provided');
+        }
+
+    }
 }
