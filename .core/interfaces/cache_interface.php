20ee6a0e0b2eb78c8e199865b4000f1e88738250
Robert Chettleburgh <rob@evosite.co.uk>
2013-07-30 19:40:07 +0100

    Add key hashing and default time outs. Create db dependency table if possible and use it, else use default time
    * Docblocks

--- .core/interfaces/cache_interface.php
+++ .core/interfaces/cache_interface.php
@@ -1,5 +1,33 @@
 <?php
 
+/**
+ * Class cache_interface
+ */
 interface cache_interface {
 
+    /**
+     * @param string $key the key to retrieve.
+     * @param array $dependencies table dependencies.
+     * @throws exception Throws exceptions if the cache node could not be connected or the key is not set.
+     * @return mixed
+     */
+    public static function get($key, array $dependencies = array('global'));
+
+    /**
+     * @param array $data associative array of key => value to be added the the cache table.
+     * @param array $dependencies table dependencies.
+     * @return bool
+     */
+    public static function set(array $data, array $dependencies = array('global'));
+
+    /**
+     * @param string $instance_id
+     * @return bool
+     * */
+    public static function connect($instance_id = '');
+
+    /**
+     * @return bool
+     */
+    public static function flush();
 }
