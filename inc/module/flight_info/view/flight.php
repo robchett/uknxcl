0972382628de4be1f7b2d8a8f40cad11bb974dea
Robert Chettleburgh <rob@evosite.co.uk>
2013-09-22 21:34:49 +0100

    Convert pure html to node

--- inc/module/flight_info/view/flight.php
+++ inc/module/flight_info/view/flight.php
@@ -1,24 +1,26 @@
 <?php
 namespace flight_info;
+
+use html\node;
+
 class flight_view extends \view {
     /** @var  controller */
     public $module;
 
     public function get_view() {
-        $html = '<div class="flight_wrapper">';
-        $html .= '<h1>' . $this->module->current->pilot_name . ' <span>' . $this->module->current->date . '</span></h1>';
-        $html .= $this->module->current->get_info();
-
+        $html = node::create('div.flight_wrapper', [],
+            node::create('h1', [], $this->module->current->pilot_name . ' ' . node::create('span', [], $this->module->current->date)) .
+            $this->module->current->get_info()
+        );
         if ($this->module->current->did > 1) {
             $html .= $this->module->current->get_stats();
-            $html .= '<div id="graph-' . $this->module->current->fid . '" class="graph"></div>';
+            $html .= node::create('div#graph-' . $this->module->current->fid . '.graph');
             \core::$inline_script[] = '
-var graph = new Graph($("#graph-' . $this->module->current->fid . '"));
-var flight = new Track(' . $this->module->current->fid . ');
-flight.add_nxcl_data(function() {graph.swap(flight)});
+            var graph = new Graph($("#graph-' . $this->module->current->fid . '"));
+            var flight = new Track(' . $this->module->current->fid . ');
+            flight.add_nxcl_data(function() {graph.swap(flight)});
             map.callback = function() {this.add_flight(' . $this->module->current->fid . ')};';
         }
-        $html .= '</div>';
         return $html;
     }
 }
