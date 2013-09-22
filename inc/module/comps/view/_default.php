926351b463e3def7117f74f6a259a2a6731c51cf
Robert Chettleburgh <rob@evosite.co.uk>
2013-09-22 19:22:51 +0100

    Convert markup to node.

--- inc/module/comps/view/_default.php
+++ inc/module/comps/view/_default.php
@@ -1,48 +1,48 @@
 <?php
 namespace comps;
+
+use html\node;
+
 class _default_view extends \view {
     public function get_view() {
-        $comps = comp_array::get_all(array('type', 'round', 'task', 'comp.title AS title', 'date', 'cid', 'comp_group.title AS class', 'file'), array('join' => array('comp_group' => 'comp.class = comp_group.cgid'), 'order' => 'date DESC, round DESC, task DESC, class ASC'));
-        $html = '';
-        $html .= '<div id="comp_wrapper">';
-        $html .= '<div id="comp_inner">';
-        $html .= '<div id="comp_list">';
-
-        $html .= '<h2>Select a Competition</h2>';
-        $html .= '<table>';
-        $html .= '
-        <thead>
-            <th>Comp</th>
-            <th>Round</th>
-            <th>Task</th>
-            <th>Class</th>
-            <th>Title</th>
-            <th>Date</th>
-            <th></th>
-        </thead>';
-        $comps->iterate(
-            function (comp $comp) use (&$html) {
-                $html .= '
-                <tr>
-                    <td>' . $comp->type . '</td>
-                    <td>Round ' . (int) $comp->round . '</td>
-                    <td>Task ' . (int) $comp->task . '</td>
-                    <td>' . $comp->class . '</td>
-                    <td>' . $comp->title . '</td>
-                    <td>' . date('d/m/Y', strtotime($comp->date)) . '</td>
-                    <td><a class="button" href="' . $comp->get_url() . '" >View</a></td>
-                </tr>';
-            }
+        $html = node::create('div#comp_wrapper div#comp_inner', [],
+            node::create('div#comp_list', [],
+                node::create('h2', [], 'Select a Competition') .
+                node::create('table', [],
+                    node::create('thead', [],
+                        node::create('th', [], 'Comp') .
+                        node::create('th', [], 'Round') .
+                        node::create('th', [], 'Task') .
+                        node::create('th', [], 'Class') .
+                        node::create('th', [], 'Title') .
+                        node::create('th', [], 'Date') .
+                        node::create('th', [])
+                    ) .
+                    node::create('tbody', [],
+                        node::nest_function(
+                            function () {
+                                $body = '';
+                                $comps = comp_array::get_all(['type', 'round', 'task', 'comp.title AS title', 'date', 'cid', 'comp_group.title AS class', 'file'], ['join' => ['comp_group' => 'comp.class = comp_group.cgid'], 'order' => 'date DESC, round DESC, task DESC, class ASC']);
+                                $comps->iterate(
+                                    function (comp $comp) use (&$body) {
+                                        $body .= node::create('tr', [],
+                                            node::create('td', [], $comp->type) .
+                                            node::create('td', [], (int) $comp->round) .
+                                            node::create('td', [], (int) $comp->task) .
+                                            node::create('td', [], $comp->class) .
+                                            node::create('td', [], $comp->title) .
+                                            node::create('td', [], date('d/m/Y', strtotime($comp->date))) .
+                                            node::create('td a.button', ['href' => $comp->get_url()], 'View')
+                                        );
+                                    }
+                                );
+                            }
+                        )
+                    )
+                ) .
+                node::create('div#comp_view')
+            )
         );
-        $html .= "</table>";
-        $html .= "</div>";
-
-
-        $html .= '<div id="comp_view">';
-        $html .= "</div>";
-
-        $html .= "</div>";
-        $html .= "</div>";
 
         $script =
             "$('#comp').on('click','#comp_list ul li a',function () {
