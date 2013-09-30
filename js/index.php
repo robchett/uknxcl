b35de42e41004f6253adce59d06a2efe7535c001
Robert Chettleburgh <rob@evosite.co.uk>
2013-09-30 19:17:11 +0100

    Set up a core JS repository and index.

--- js/index.php
+++ js/index.php
@@ -1,13 +1,3 @@
 <?php
-
-header('Content-type: text/javascript');
-
-$output = "";
-$files = glob("./*.js", GLOB_MARK);
-foreach ($files as $file) {
-    $output .= file_get_contents($file);
-}
-//$output = preg_replace('/(\s|;|^|,)\/\/.*$/m','$1',$output);
-//$output = preg_replace('/\s+/',' ',$output);
-
-echo $output;
+$core_files = ['jquery', '_ajax','_default', 'colorbox'];
+include($_SERVER['DOCUMENT_ROOT'] . '/.core/js/index.php');
