86d88d11ecf6552729eed136e4bdc7ab21d06028
Robert Chettleburgh <rob@evosite.co.uk>
2013-09-30 19:01:23 +0100

    Add basic stats.

--- inc/module/stats/view/_default.php
+++ inc/module/stats/view/_default.php
@@ -1,9 +1,17 @@
 <?php
+namespace stats;
+class _default_view extends \view {
 
-class _default_view extends view {
-
-    /** @return html\node */
+    /** @return \html\node */
     public function get_view() {
-        return $this->module->page_object->body;
+        \core::$inline_script[] = '
+        var $graph = new Graph($("#graph_contain"), 600);
+        var obj = ' . $this->module->get_stats() . '
+        obj.size = function() {
+            return this.nxcl_data.track.length;
+        }
+        $graph.swap(obj);
+        ';
+        return $this->module->page_object->body . \html\node::create('div#graph_contain');
     }
 }
