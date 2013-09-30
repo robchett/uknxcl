a7179f0fe76c542ce0a9ca686ebac375c6e5824f
Robert Chettleburgh <rob@evosite.co.uk>
2013-09-30 19:17:11 +0100

    Set up a core JS repository and index.

--- .core/js/_default.js
+++ .core/js/_default.js
@@ -16,6 +16,31 @@ function page_callback(json) {
     }
 };
 
+recreate_checkboxes = function () {
+    $(".checkbox_replace").each(function () {
+        var $this = $(this);
+        var $input = $this.children("input");
+        if ($input.prop('checked')) {
+            $this.addClass("checked");
+        }
+    });
+    $(".checkbox_replace").click(function () {
+        var $this = $(this);
+        var $input = $this.children("input");
+        if (!$input.prop('checked')) {
+            $this.addClass("checked");
+            $input.prop('checked', true);
+        } else {
+            $this.removeClass("checked");
+            $input.prop('checked', false);
+        }
+    });
+};
+$(document).ready(function () {
+    recreate_checkboxes();
+    $.fn.ajax_factory.defaults.complete.push('recreate_checkboxes')
+});
+
 function toggle_page($page) {
     if ($page.css('z-index') != 2) {
         var $main = $('#main');
