47c7b5589d3d9020bb7acd51ba2e268009e5a52c
Robert Chettleburgh <rob@evosite.co.uk>
2013-09-30 19:02:28 +0100

    Add count and setup live and deleted flags.

--- .core/classes/db/select.php
+++ .core/classes/db/select.php
@@ -2,6 +2,9 @@
 namespace db;
 class select extends query {
 
+    /**
+     * @return \PDOStatement
+     */
     public function execute() {
         $query = 'SELECT ' . implode(', ', $this->fields) . ' FROM ' . $this->table . $this->get_joins() . $this->get_filters() . $this->get_groupings() . ' ' . $this->get_limit();
         return \db::query($query, $this->parameters);
