79f8d6ff0488b06026253f1015ce1d60365b0ff0
Robert Chettleburgh <rob@evosite.co.uk>
2013-10-07 19:59:32 +0100

    Add creation of mlinks

--- .core/module/cms/form/add_field_form.php
+++ .core/module/cms/form/add_field_form.php
@@ -12,23 +12,47 @@ use module\cms\object\field_type;
  */
 abstract class add_field_form extends form {
 
+    public $link_module;
     public $mid;
     public $title;
     public $type;
 
     public function __construct() {
-        parent::__construct(array(
+        parent::__construct([
                 form::create('field_string', 'title'),
                 form::create('field_string', 'field_name'),
                 form::create('field_link', 'type')->set_attr('link_module', '\module\cms\object\field_type')->set_attr('link_field', 'title'),
-                form::create('field_int', 'mid')
-            )
+                form::create('field_link', 'link_module')->set_attr('link_module', '\module\cms\object\_cms_module')->set_attr('link_field', 'title'),
+                form::create('field_link', 'link_field')->set_attr('link_module', '\module\cms\object\_cms_field')->set_attr('link_field', 'title'),
+                form::create('field_int', 'mid')->set_attr('hidden', true)
+            ]
         );
+        $this->id = 'add_field_form';
+    }
 
-        $this->get_field_from_name('mid')->set_attr('hidden', true);
+    public function get_html() {
+        \core::$inline_script[] = '
+            var $form = $("#add_field_form");
+            $form.find("#add_field_form_field_link_module").hide();
+            $form.find("#add_field_form_field_link_field").hide();
+            $form.find("[name=type]").change(function() {
+                if($(this).val() == 3 || $(this).val() == 4) {
+                     $form.find("#add_field_form_field_link_module").show();
+                     $form.find("#add_field_form_field_link_field").show();
+                } else {
+                     $form.find("#add_field_form_field_link_module").hide();
+                     $form.find("#add_field_form_field_link_field").hide();
+                }
+            });
+        ';
+        return parent::get_html();
     }
 
     public function do_submit() {
+        if ($_REQUEST['type'] == 3) {
+            $this->get_field_from_name('link_field')->required = true;
+            $this->get_field_from_name('link_module')->required = true;
+        }
         if (parent::do_submit()) {
             $module = new _cms_module([], $this->mid);
             $field = new field_type([], $this->type);
@@ -38,14 +62,31 @@ abstract class add_field_form extends form {
             if ($inner = $field_type->get_database_create_query()) {
                 db::query('ALTER TABLE ' . $module->table_name . ' ADD ' . $this->field_name . ' ' . $field_type->get_database_create_query(), array(), 1);
             }
+
+            if ($field->title == 'mlink') {
+                $source_module = new \module\cms\object\_cms_module(['table_name', 'primary_key'], $this->link_module);
+                db::create_table($module->table_name . '_link_' . $source_module->table_name, [
+                        '_link_id' => 'SMALLINT NOT NULL AUTO_INCREMENT',
+                        $module->primary_key => 'SMALLINT NOT NULL',
+                        'link_' . $source_module->primary_key => 'SMALLINT NOT NULL',
+                        'fid' => 'SMALLINT NOT NULL',
+                    ], ['PRIMARY KEY (`_link_id`)']
+                );
+            }
+
             $res = db::select('_cms_field')->retrieve('MAX(position) AS pos')->filter_field('mid', $this->mid)->execute()->fetchObject();
-            db::insert('_cms_field')
+            $insert = db::insert('_cms_field')
                 ->add_value('title', $this->title)
                 ->add_value('type', $field->title)
                 ->add_value('field_name', $this->field_name)
                 ->add_value('mid', $this->mid)
-                ->add_value('position', $res->pos + 1)
-                ->execute();
+                ->add_value('position', $res->pos + 1);
+            if($field->title == 'link' || $field->title == 'mlink') {
+                $insert->add_value('link_module', $this->link_module)
+                    ->add_value('link_field', $this->link_field);
+            }
+            $insert->execute();
+
             $obj = $module->get_class();
             $obj->mid = $this->mid;
             ajax::update($obj->get_cms_edit_module()->get());
