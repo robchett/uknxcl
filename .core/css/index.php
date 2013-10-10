cf659f2bd72c3b16343694a3559445355a8b5d7c
Robert Chettleburgh <rob@evosite.co.uk>
2013-10-10 20:58:43 +0100

    Compile less to css and cache.

--- .core/css/index.php
+++ .core/css/index.php
@@ -4,17 +4,30 @@ define('load_core', false);
 include($_SERVER['DOCUMENT_ROOT'] . '/index.php');
 $css = '';
 $files = glob(root . '/.core/css/*.less');
+$time_stamp = 0;
 foreach ($files as $file) {
     $css .= '@import "' . pathinfo($file, PATHINFO_FILENAME) . "\";\n";
+    if (($time = filemtime($file)) > $time_stamp) {
+        $time_stamp = $time;
+    }
 }
 $files = glob(root . '/css/*.less');
 foreach ($files as $file) {
     $css .= '@import "' . pathinfo($file, PATHINFO_FILENAME) . "\";\n";
+    if (($time = filemtime($file)) > $time_stamp) {
+        $time_stamp = $time;
+    }
 }
-\classes\lessc::$debug = true;
-$lessc = new \classes\lessc();
-$lessc->addImportDir(root . '/css/');
-$lessc->addImportDir(root . '/.core/css/');
-$less = $lessc->parse($css, null, 'dynamic_import.less');
+$file_path = root . '/.cache/' . $time_stamp . '.css';
+if (file_exists($file_path)) {
+    echo file_get_contents($file_path);
+} else {
+    \classes\lessc::$debug = true;
+    $lessc = new \classes\lessc();
+    $lessc->addImportDir(root . '/css/');
+    $lessc->addImportDir(root . '/.core/css/');
+    $less = $lessc->parse($css, null, 'dynamic_import.less');
 //$less = lessc::cexecute($css, null);
-echo $less;
+    file_put_contents($file_path, $less);
+    echo $less;
+}
