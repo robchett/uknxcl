c437d9463077d3f2ef57b5cbbbeef1f87d2a6bf4
Robert Chettleburgh <rob@evosite.co.uk>
2013-10-11 19:49:20 +0100

    Wrap all in try cache block

--- index.php
+++ index.php
@@ -1,2 +1,6 @@
 <?php
-include $_SERVER['DOCUMENT_ROOT'] . '/.core/config.php';
+try {
+    include $_SERVER['DOCUMENT_ROOT'] . '/.core/config.php';
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
