bb137e01a7b02959de47a54210a726a97d9bfdc8
Robert Chettleburgh <rob@evosite.co.uk>
2013-10-16 20:05:28 +0100

    Add default nav to cms pages except login

--- .core/module/cms/view/cms_view.php
+++ .core/module/cms/view/cms_view.php
@@ -2,6 +2,7 @@
 namespace core\module\cms\view;
 
 use core\classes\view;
+use core\core;
 
 /**
  * Class cms_view
@@ -13,6 +14,7 @@ abstract class cms_view extends view {
      * @return \html\node
      */
     public function get() {
+        core::$page_config->pre_content = $this->module->get_main_nav();
         return $this->get_view()->get();
     }
 
