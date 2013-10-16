394864678171ae72875ec4077b9cb16a3d44151e
Robert Chettleburgh <rob@evosite.co.uk>
2013-10-16 20:10:59 +0100

    Move required js to extend.

---
+++ inc/core.php
@@ -0,0 +1,8 @@
+<?php
+class core extends \core\core {
+public function __construct() {
+        self::$js[] = 'http://maps.google.com/maps/api/js?libraries=geometry&amp;sensor=false';
+        self::$js[] = 'https://www.google.com/jsapi';
+parent::__construct();
+}
+}
