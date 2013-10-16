085fa5606a7619b91f2e6844119fd9593586d868
Robert Chettleburgh <rob@evosite.co.uk>
2013-10-16 20:02:45 +0100

    move error handler out of config.

--- .core/config.php
+++ .core/config.php
@@ -29,22 +29,8 @@ include(root . '/.core/classes/auto_loader.php');
 include(root . '/.core/dependent/classes/auto_loader.php');
 $auto_loader = new \classes\auto_loader();
 
-set_error_handler(function ($errno, $errstr, $errfile, $errline) {
-        if (strpos($errfile, 'xdebug') !== 0) {
-            if(function_exists('xdebug_break')){
-                xdebug_break();
-            }
-            require_once(root . '/.core/core.php');
-            $error = '<div class="error_message mysql"><p>Error #' . $errno . ' "' . $errstr . '" in ' . $errfile . ' on line ' . $errline . '</p>' . \core::get_backtrace() . '</div>';
-            if (ajax) {
-                require_once(root . '/.core/classes/ajax.php');
-                \classes\ajax::inject('body', 'append', $error);
-            } else {
-                echo $error;
-            }
-        }
-    }
-);
+set_error_handler(['\classes\error_handler', 'handle_error']);
+
 if (!defined('load_core') || load_core) {
     include(core_dir . '/core.php');
     $core = new core();
