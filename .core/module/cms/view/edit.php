0d217c1b38dd870bc3f1979ae950a68d52bd170a
Robert Chettleburgh <rob@evosite.co.uk>
2013-10-16 20:06:51 +0100

    Fix filters Add sub modules delete

--- .core/module/cms/view/edit.php
+++ .core/module/cms/view/edit.php
@@ -11,7 +11,8 @@ abstract class edit extends cms_view {
     public function get_view() {
         $html = node::create('div', [],
             node::create('h2', [], 'Edit a ' . get_class($this->module->current)) .
-            $this->module->current->get_cms_edit()
+            $this->module->current->get_cms_edit() .
+            ($this->module->current->get_primary_key() ? $this->module->get_sub_modules() : '')
         );
         return $html;
     }
