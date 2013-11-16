b0048b83ee177f838216b66940c4fc114c47b231
Rob <robchett@gmail.com>
2013-11-16 17:58:32 +0000

    Reformat

--- inc/core.php
+++ inc/core.php
@@ -1,8 +1,10 @@
 <?php
 class core extends \core\core {
-public function __construct() {
+
+    public function __construct() {
         self::$js[] = 'http://maps.google.com/maps/api/js?libraries=geometry&amp;sensor=false';
         self::$js[] = 'https://www.google.com/jsapi';
-parent::__construct();
-}
+
+        parent::__construct();
+    }
 }
