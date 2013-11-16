fba23d0a88723a96bc1ac82b5bdcd799bdb68d7f
Rob <robchett@gmail.com>
2013-11-16 18:00:39 +0000

    Add the google map instansiation to teh template

--- inc/template/html.php
+++ inc/template/html.php
@@ -1,7 +1,22 @@
 <?php
 namespace template;
 
+use classes\module;
+
 class html extends \core\template\html {
+
+    public function __construct(module $module) {
+        \core::$global_script[] = 'var map';
+        \core::$inline_script[] = '
+map = new UKNXCL_Map($("#map_wrapper"));
+if (typeof google != \'undefined\') {
+    map.load_map();
+} else {
+    $(\'#map\').children(\'p.loading\').html(\'Failed to load Google resources\');
+}
+';
+        parent::__construct($module);
+    }
 
 }
  
