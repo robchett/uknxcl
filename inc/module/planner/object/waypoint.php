0b7c9fcbab4acf5ceda221d16ef08f0fc6bbbb8b
Rob <robchett@gmail.com>
2013-11-28 19:12:21 +0000

    use new get_all format.

--- inc/module/planner/object/waypoint.php
+++ inc/module/planner/object/waypoint.php
@@ -18,8 +18,10 @@ class waypoint extends table {
      * @param array $options
      * @return waypoint_array
      */
-    public static function get_all(array $fields, array $options = array()) {
-        return waypoint_array::get_all($fields, $options);
+    public static function get_all(array $fields, array $options = []) {
+        $waypoint_array = new waypoint_array();
+        $waypoint_array->get_all(get_called_class(), $fields, $options);
+        return $waypoint_array;
     }
 
     /**
