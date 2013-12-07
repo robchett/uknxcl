a1dadf496bcdf48962756d71b3d279bef1c32df8
Rob <robchett@gmail.com>
2013-12-07 13:24:43 +0000

    Use of heredoc for JS, use node:::create for html and re arrange the call order for map creation so the callbacks can be set.

--- inc/template/html.php
+++ inc/template/html.php
@@ -3,20 +3,16 @@ namespace template;
 
 use classes\ajax;
 use classes\module;
+use html\node;
 
 abstract class html extends \core\template\html {
 
     public function __construct(module $module) {
         if (!ajax) {
-            \core::$global_script[] = 'var map';
-            \core::$inline_script[] = '
-map = new UKNXCL_Map($("#map_wrapper"));
-if (typeof google != \'undefined\') {
-    map.load_map();
-} else {
-    $(\'#map\').children(\'p.loading\').html(\'Failed to load Google resources\');
-}
-';
+            \core::$inline_script[] =
+                <<<js
+                map = new UKNXCL_Map($("#map_wrapper"));
+js;
         }
         parent::__construct($module);
     }
@@ -24,42 +20,51 @@ if (typeof google != \'undefined\') {
     public function get() {
         if (!ajax) {
             \core::$inline_script[] = 'loaded_modules = {"' . uri . '":true};';
-            return '
-<div id="left_col">
-    <div id="nav">
-        <ul>
-            ' . \core::$singleton->module->get_main_nav() . '
-        </ul>
-    </div>
-    <div id="main">
-        <div id="' . $this->get_page_selector() . '" data-url="' . (isset($_POST['url']) ? $_POST['url'] : uri) . '">
-            ' . $this->get_view() . '
-        </div>
-    </div>
-</div>
-<div id="map_wrapper">
-    <div id="waypoint_mode_help" style="display:none">You are in waypoint mode</div>
-    <div id="map_interface">
-        <div id="map_interface_padding">
-            <div id="graph_wrapper"></div>
-            <div id="slider">
-            </div>
-            <div id="controls">
-                <input id="play" type="submit" value="play" onclick="map.play()"/>
-                <input id="pause" type="submit" value="pause" onclick="map.pause()"/>
-                <a id="slider_time">00:00</a>
-            </div>
-        </div>
-    </div>
-    <div id="map_interface_3d">
-        <span class="show">Show</span>
-        <span class="hide">Hide</span>
-
-        <div id="tree_content"><a href="#" title="Load Airspace" class="load_airspace button" onclick="map.load_airspace();">Load Airspace</a></div>
-    </div>
-    <div id="map"><p class="loading">Google Maps are loading...</p></div>
-    <div id="map3d"><p class="loading">Google Earth is loading...</p></div>
-</div>';
+            $html =
+                node::create('div#left_col', [],
+                    node::create('div#nav ul', [], \core::$singleton->module->get_main_nav()) .
+                    node::create('div#main', [],
+                        node::create('div#' . $this->get_page_selector(), ['data-uri' => (isset($_POST['url']) ? $_POST['url'] : uri)],
+                            $this->get_view()
+                        )
+                    )
+                ) .
+                node::create('div#map_wrapper', [],
+                    node::create('div#waypoint_mode_help', ['style' => 'display:none'], 'You are in waypoint mode') .
+                    node::create('div#map_interface', [],
+                        node::create('div#map_interface_padding', [],
+                            node::create('div#graph_wrapper', [], '') .
+                            node::create('div#slider', [], '') .
+                            node::create('div#controls', [],
+                                node::create('input#play', ['value' => 'play', 'type' => 'submit', 'onclick' => "map.play()"], '') .
+                                node::create('input#pause', ['value' => 'pause', 'type' => 'submit', 'onclick' => "map.pause()"], '') .
+                                node::create('a#slider_time', [], '00:00')
+                            )
+                        )
+                    ) .
+                    node::create('div#map_interface_3d', [],
+                        node::create('span.show', [], 'Show') .
+                        node::create('span.hide', [], 'Hide') .
+                        node::create('div#tree_content', [],
+                            node::create('a.load_airspace.button', ['href' => '#', 'onclick' => 'map.load_airspace();'], 'Load Airspace')
+                        )
+                    ) .
+                    node::create('div#map p.loading', [], 'Google Maps are loading...') .
+                    node::create('div#map3d p.loading', [], 'Google Earth is loading...')
+                );
+            \core::$global_script[] =
+                <<<js
+                var map;
+js;
+            \core::$inline_script[] =
+                <<<js
+                if (typeof google != 'undefined') {
+    map.load_map();
+} else {
+    $('#map').children('p.loading').html('Failed to load Google resources');
+}
+js;
+            return $html;
         } else {
             $content = $this->get_view();
             ajax::inject('#main', 'append', '<div id="' . $this->get_page_selector() . '" data-url="' . (isset($_POST['url']) ? $_POST['url'] : uri) . '">' . $content . '</div>', '#' . $this->get_page_selector());
