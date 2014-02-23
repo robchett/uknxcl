d1b5386cdde6e3eea72a86b1a00e8cf363336c64
Rob <robchett@gmail.com>
2014-02-23 12:55:13 +0000

    Correct the flight info page

--- inc/module/flight_info/view/flight.php
+++ inc/module/flight_info/view/flight.php
@@ -11,11 +11,9 @@ class flight extends \template\html {
 
     public function get_view() {
         $html = node::create('div.flight_wrapper', [],
-            node::create('h1', [], $this->module->current->pilot->name . ' ' . node::create('span', [], $this->module->current->date)) .
+            node::create('h1', [], $this->module->current->pilot->name . ' ' . node::create('span', [], date('Y-m-d', $this->module->current->date))) .
             $this->module->current->get_info() .
-            ($this->module->current->did > 1 ?
-                $this->module->current->get_stats() . node::create('div#graph-' . $this->module->current->fid . '.graph') : '')
-
+            ($this->module->current->did > 1 && $this->module->current->track->parsed ? $this->module->current->get_stats() . node::create('div#graph-' . $this->module->current->fid . '.graph') : '')
         );
         if ($this->module->current->did > 1) {
             \core::$inline_script[] = '
