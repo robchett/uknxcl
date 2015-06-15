969d7b5f93bfde5ee9e872432790a261da63f7ee
Robert Chettleburgh <robchett@gmail.com>
2015-06-15 22:07:45 +0100

    Pull correct data from flight object

---
+++ inc/stubs/statistics_set.php
@@ -0,0 +1,14 @@
+<?php
+
+class statistics_set {
+
+    /** @return statistic */
+    function climb() { }
+
+    /** @return statistic */
+    function speed() { }
+
+    /** @return statistic */
+    function height() { }
+
+}
