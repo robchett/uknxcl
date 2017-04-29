3f941ca8377c4140eefe961b074a3ffdea06f3b7
Rob Chett <robchett@gmail.com>
2017-04-29 16:19:22 +0100

    Convert converter to twig

--- inc/module/converter/view/_default.php
+++ inc/module/converter/view/_default.php
@@ -4,22 +4,19 @@ namespace module\converter\view;
 use classes\view;
 use html\node;
 use module\converter\form;
+use traits\twig_view;
 
 class _default extends \template\html {
+    use twig_view;
 
-    public function get_view() {
-        $html = node::create('div', [],
-            node::create('h1.page-header', [], 'UKNXCL Conversion Tools') .
-            node::create('p', [], 'Enter lat/lng values as decimal or space separated for seconds') .
-            $this->get_coordinate_converter()
-        );
-        return $html;
+    function get_template_file() {
+        return 'inc/module/converter/view/_default.twig';
     }
 
-    public function get_coordinate_converter() {
-        $form = new form\coordinate_conversion_form();
-        $form->wrapper_class[] = 'callout';
-        $form->wrapper_class[] = 'callout-primary';
-        return $form->get_html();
+    function get_template_data() {
+        $form1 = new form\coordinate_conversion_form();
+        return [
+            'form_1' => $form1->get_html()->get(),
+        ];
     }
 }
