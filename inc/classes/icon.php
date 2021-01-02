9e46e4345d0529d169b209e1a42ddd46755b073b
Rob Chett <robchett@gmail.com>
2021-01-02 18:13:59 +0000

    Fix flight toggle toggle

--- inc/classes/icon.php
+++ inc/classes/icon.php
@@ -7,8 +7,10 @@ use html\node;
 class icon {
 
     public static function get($icon, $tag = 'span', $attributes = []): string {
+        $attributes['class'][] = 'glyphicon';
+        $attributes['class'][] = 'glyphicon-' . $icon;
         $attr_string = node::get_attributes($attributes);
-        return "<$tag class='glyphicon glyphicon-$icon' $attr_string></$tag>";
+        return "<$tag $attr_string></$tag>";
     }
 
 }
