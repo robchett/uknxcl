aa01df3e7b1269ccb1c7fff9d87679113225ec36
Rob Chett <robchett@gmail.com>
2021-10-09 15:00:12 +0100

    Fix email sending (and therefor the flight submission)

--- inc/classes/email.php
+++ inc/classes/email.php
@@ -6,7 +6,8 @@ use Aws\Result;
 use Aws\Ses\SesClient;
 use Exception;
 
-class email {
+class email
+{
 
     const METHOD_SENDMAIL = 1;
     const METHOD_SES = 2;
@@ -27,21 +28,25 @@ class email {
      * @param string[] $cc
      * @param string[] $bcc
      */
-    public function set_recipients(array $base, array $cc = [], array $bcc = []): void {
+    public function set_recipients(array $base, array $cc = [], array $bcc = []): void
+    {
         $this->recipients[''] = $base;
         $this->recipients['cc'] = $cc;
         $this->recipients['bcc'] = $bcc;
     }
 
-    public function set_subject(string $subject): void {
+    public function set_subject(string $subject): void
+    {
         $this->subject = $subject;
     }
 
-    public function set_content(string $content): void {
+    public function set_content(string $content): void
+    {
         $this->content = $content;
     }
 
-    public function load_template(string $file): void {
+    public function load_template(string $file): void
+    {
         if (!file_exists($file)) {
             throw new Exception('Email template not found');
         } else {
@@ -49,45 +54,50 @@ class email {
         }
     }
 
-    public function send(): Result|bool|null {
+    public function send(): Result|bool|null
+    {
         $content = $this->do_replace($this->content);
         if (static::$sending_method == static::METHOD_SENDMAIL) {
             return mail(implode(',', $this->recipients['']), $this->do_replace($this->subject), $content);
         } else if (static::$sending_method == static::METHOD_SES) {
-            $controller = new SesClient([
-                'version' => 'latest',
-                'profile' => getenv('awsProfile') ?: 'uknxcl',
-                'region'  => getenv('awsRegion') ?: 'aws-west-1',
-            ]);
-            return $controller->sendEmail([
-                'Source'      => getenv('emailFromAddress') ?: 'robchett@gmail.com',
-                'Destination' => [
-                    'ToAddresses'  => $this->recipients[''],
-                    'CcAddresses'  => $this->recipients['cc'],
-                    'BccAddresses' => $this->recipients['bcc'],
-                ],
-                'Message'     => [
-                    'Subject' => [
-                        'Data'    => $this->subject,
-                        'Charset' => 'UTF8',
+            try {
+                $controller = new SesClient([
+                    'version' => 'latest',
+                    'profile' => getenv('awsProfile') ?: 'uknxcl',
+                    'region'  => getenv('awsRegion') ?: 'eu-west-1',
+                ]);
+                return $controller->sendEmail([
+                    'Source'      => getenv('emailFromAddress') ?: 'robchett@gmail.com',
+                    'Destination' => [
+                        'ToAddresses'  => $this->recipients[''],
+                        'CcAddresses'  => $this->recipients['cc'],
+                        'BccAddresses' => $this->recipients['bcc'],
                     ],
-                    'Body'    => [
-                        'Text' => [
-                            'Data'    => '',
+                    'Message'     => [
+                        'Subject' => [
+                            'Data'    => $this->subject,
                             'Charset' => 'UTF8',
                         ],
-                        'Html' => [
-                            'Data'    => $content,
-                            'Charset' => 'UTF8',
+                        'Body'    => [
+                            'Text' => [
+                                'Data'    => '',
+                                'Charset' => 'UTF8',
+                            ],
+                            'Html' => [
+                                'Data'    => $content,
+                                'Charset' => 'UTF8',
+                            ],
                         ],
                     ],
-                ],
-            ]);
+                ]);
+            } catch (\Exception) {
+            }
         }
         return null;
     }
 
-    protected function do_replace(string $string): string {
+    protected function do_replace(string $string): string
+    {
         return str_replace(array_keys($this->replacements), array_values($this->replacements), $string);
     }
 }
